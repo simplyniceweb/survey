34142786998e6fdb486963a5a19f56c3aac11cc4
simplynice25 <simplyniceweb@gmail.com>
2014-02-08 21:03:02 +0800

    login design

--- application/views/login.php
+++ application/views/login.php
@@ -6,54 +6,100 @@
 	<title>-::Web Based Generator::-</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/styles/design.css"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css"/>
+	<style>
+	.page-header {
+		border-bottom: 1px solid #FFFFFF;
+		margin: 20px 0;
+		padding-bottom: 9px;
+		text-align: center;
+	}
+
+	.form-control {
+		border: 1px solid #D6D6D6;
+		border-radius: 0;
+		box-shadow: none;
+		height: 50px;
+		padding: 6px 15px;
+	}
+
+	body {
+		background-color: #ee7778;
+	}
+
+	.row {
+		background: #ededed;
+		padding: 20px 50px 20px 50px;
+		margin: 100px;
+	}
+
+	legend {
+		border: medium none;
+		color: #7F7F7F;
+		display: block;
+		font-size: 20px;
+		line-height: inherit;
+		margin-bottom: 15px;
+		padding: 0;
+		text-align: center;
+		width: 100%;
+	}
+
+	.btn {
+		padding: 10px;
+		border-radius: 0;
+		border: none;
+		font-size: 21px;
+	}
+	</style>
 </head>
 <body>
-
-<div id="container">
-
-	<div class="row">
-		<div class="col-sm-4 col-md-offset-4">
-
-		<?php if(isset($_GET['login']) && $_GET['login'] == "false"): ?>
-		<div class="list-group-item">
-			<div class="alert alert-danger">
-				<small>Please provide the right credentials.</small>
-			</div>
-		</div>
-		<?php endif; ?>
-		
-		<?php if(isset($_GET['ban']) && $_GET['ban'] == "true"): ?>
-		<div class="list-group-item">
-			<div class="alert alert-danger">
-				<small>An adminastrator blocked you from viewing this system.</small>
-			</div>
-		</div>
-		<?php endif; ?>
-		
-		<?php echo form_open('index/verify'); ?>
-			<legend><h4>LOGIN</h4></legend>
-
-			<div class="list-group-item">
-				<label><small>Email Address</small></label>
-				<input type="email" name="user_email" class="form-control" required="required" placeholder="Email Address">
-			</div>
-
-			<div class="list-group-item">
-				<label><small>Password</small></label>
-				<input type="password" name="user_password" class="form-control" required="required" placeholder="Password">
-			</div>
-			
-			<div class="list-group-item">
-				<button class="btn btn-primary btn-block">
-					<small>Log In</small>
-				</button>
-			</div>
-			
-			<a class="col-md-offset-4" href="register">No account yet? Register!</a>
-		<?php echo form_close(); ?>
+<div class="container">
+<div class="row">
+	<?php if(isset($_GET['login']) && $_GET['login'] == "false"): ?>
+	<div class="list-group-item">
+		<div class="alert alert-danger">
+			<small>Please provide the right credentials.</small>
 		</div>
 	</div>
+	<?php endif; ?>
 
-</div>
+	<?php if(isset($_GET['ban']) && $_GET['ban'] == "true"): ?>
+	<div class="list-group-item">
+		<div class="alert alert-danger">
+			<small>An adminastrator blocked you from viewing this system.</small>
+		</div>
+	</div>
+	<?php endif; ?>
+  <div class="page-header"><h2>Login Form</h2></div>
+	<?php echo form_open('index/verify',array('class' => 'form-horizontal')); ?>
+    <fieldset>
+
+      <!-- Form Name -->
+      <legend><h3>No account yet? <a href="register">Register!</a></h3></legend>
+
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-1 control-label" for="username"></label>  
+        <div class="col-md-12">
+          <input id="username" name="user_email" type="text" required="required" placeholder="Email Address" class="form-control input-md">
+        </div>
+      </div>
+
+      <!-- Password input-->
+      <div class="form-group">
+        <label class="col-md-1 control-label" for="password"></label>
+        <div class="col-md-12">
+          <input id="password" name="user_password" type="password" required="required" placeholder="Password" class="form-control input-md">
+        </div>
+      </div>
+
+      <!-- Button (Double) -->
+      <div class="form-group">
+        <label class="col-md-1 control-label" for="login"></label>
+      <div class="col-md-12">
+        <button id="login" name="login" class="btn btn-block btn-success">Login</button>
+      </div>
+    </fieldset>
+  </form>
 </body>
 </html>
